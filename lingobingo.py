22e90fdc0d9afe32e580a2b0a251805bc9c82991
Brandon Phillips <bericp1@gmail.com>
2014-10-08 16:24:50 -0600

    mongo testing

--- lingobingo.py
+++ lingobingo.py
@@ -6,15 +6,33 @@ Project 1
 Lingo Bingo
 '''
 
-import os
+import os, sys
 from flask import Flask
+from mongoengine import *
+
 app = Flask(__name__, static_url_path='')
+
+mongodb_url = os.environ.get('MONGOLAB_URI', False)
+if mongodb_url: connect('lingobingo', host=mongodb_url)
+else: connect('lingobingo')
+
+class TestDoc(DynamicDocument): pass
+
+testDoc = TestDoc()
+testDoc.testString = 'test'
+testDoc.testInt = 1
+testDoc.testFloat = 1.1
+testDoc.save()
 
 @app.route('/')
 def home_serve():
     return app.send_static_file('index.html')
 
+@app.route('/test')
+def test():
+    return str(TestDoc.objects().count())
+
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    debug = os.environ.get("APP_ENV", "development") == "development"
+    port = int(os.environ.get('PORT', 5000))
+    debug = os.environ.get('APP_ENV', 'development') == 'development'
     app.run(host='0.0.0.0', port=port, debug=debug)
